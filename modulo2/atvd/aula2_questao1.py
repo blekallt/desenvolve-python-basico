e3ce4a13dadfa9e5b6640cf0882ae608ece83042
blekallt <kaikalves1208@gmail.com>
2026-02-25 21:10:11 -0300

    Add files via upload

---
+++ modulo2/atvd/aula2_questao1.py
@@ -0,0 +1,8 @@
+valor1 = 5
+valor2 = 2
+print(type(valor1))
+print(type(valor2))
+print(valor1 / valor2)
+valor3 = valor1 / valor2
+print(valor3)
+print(type(valor3))
---
+++ modulo2/atvd/aula2_questao2.py
@@ -0,0 +1,13 @@
+resultado = "o resultado é"
+v1 = 10
+v2 = 3.5
+print(v1 + v2)
+v3 = v1 + v2
+print(resultado)
+print(type(resultado))
+print(v1)
+print(type(v1))
+print(v2)
+print(type(v2))
+print(v3)
+print(type(v3))
