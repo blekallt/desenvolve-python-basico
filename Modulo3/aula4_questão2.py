5954cdd32cb95740d69b0b4f4c8dc63a88775e6d
Kaik <kaikalves1208@gmail.com>
2026-04-20 19:13:21 -0300

    Add movie rating evaluation script

---
+++ Modulo3/aula4_questão2.py
@@ -0,0 +1,12 @@
+avaliaçao = int(input("Insira a avaliação do filme em uma escala de 1 a 5: "))
+
+if avaliaçao == 5:
+    print("Excelente!")
+elif avaliaçao == 4:
+    print("Muito bom!")
+elif avaliaçao == 3:
+    print("Bom")
+elif avaliaçao == 2:
+    print("Regular")
+elif avaliaçao == 1:
+    print("Ruim")
